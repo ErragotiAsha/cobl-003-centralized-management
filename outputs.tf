84aa0aed2d15ff5a8e25c81b34b38fced0f80eb5
ErragotiAsha <ashasekhar.2004@gmail.com>
2025-12-15 15:14:35 +0530

    Fix folder structure: move Terraform files into terraform folder

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "central_s3_bucket_arn" {
-  description = "ARN of the centralized S3 bucket"
-  value       = aws_s3_bucket.central_management_bucket.arn
-}
-
-output "member_roles_arns" {
-  description = "ARNs of the IAM roles created in Member Accounts"
-  value       = [for r in aws_iam_role.central_access_role : r.arn]
-}
